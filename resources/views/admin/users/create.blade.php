17a9d6cfdeffc76da89ee5923c47920c3244abe7
Web 02 <web02@creativetechnet.local>
2019-02-14 17:28:37 +0500

    users page working without photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1> Create User </h1>
+    {!! Form::open(['method' => 'post', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
+
+    <div class="form-group">
+        {!! form::label('name', 'Name:') !!}
+        {!! form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! form::label('email', 'Email:') !!}
+        {!! form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! form::label('password', 'Password:') !!}
+        {!! form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
+
+    <div class="form-group">
+        {!! form::label('role_id', 'Role:') !!}
+        {!! form::select('role_id', ['' => 'Choose a Role'] + $roles , null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! form::label('is_active', 'is_active:') !!}
+        {!! form::select('is_active', array(1 => 'Active' , 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! form::label('file', 'file') !!}
+        {!! form::file('file', null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+    </div>
+
+
+    {!! Form::close() !!}
+
+
+    @include('includes.form_error')
+
+@stop
+
